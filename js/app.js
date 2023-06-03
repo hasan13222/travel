d8b6b514ed617cda91f87e47314cdf377882f5e9
hasan13222 <jamil8305@gmail.com>
2023-06-03 23:06:10 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$('.owl-carousel.carousel_ad').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:false,
+    autoplayTimeout:1000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+$('.owl-carousel.carousel_deals').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:false,
+    autoplayTimeout:1000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+});
