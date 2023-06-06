f51ead4380087a814b57c174bbc792e9bd84229e
hasan13222 <jamil8305@gmail.com>
2023-06-06 23:43:46 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ $('.owl-carousel.carousel_ad').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-    autoplay:false,
-    autoplayTimeout:1000,
+    autoplay:true,
+    autoplayTimeout:3000,
+    smartSpeed: 1500,
     autoplayHoverPause:false,
     responsive:{
         0:{
@@ -22,18 +23,73 @@ $('.owl-carousel.carousel_deals').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-    autoplay:false,
-    autoplayTimeout:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
     autoplayHoverPause:false,
+    slideBy: 4,
     responsive:{
         0:{
-            items:4
+            items:1
         },
-        600:{
-            items:4
+        768:{
+            items:3
         },
         1000:{
             items:4
         }
     }
 });
+
+
+
+$(document).ready(function(){
+    
+    // toggle login button
+    $(".menu").click(function(){
+      $(".t_box").toggle();
+    });
+
+
+    // hero options toggle button
+    $(".flight_details .item").click(function(){
+      $(this).find(".toggle_box").toggle();
+    });
+
+
+    // chat box toggle button
+    $(".chat_box .chat_icon").click(function(){
+        $(this).toggleClass("cross");
+        $(this).find(".fa-message").toggleClass("fa-plus");
+        $(this).find(".fa-message").toggleClass("fa-xmark");
+        $(".chat").toggle();
+        $(".chat_desk .btns").toggleClass("flx");
+        $(".chat_desk .btns .chat_btn").toggleClass("active");
+
+        // toggle chat and help
+        $(".chat_btn").click(function(){
+            $(".chat").show();
+            $(".help").hide();
+        });
+        $(".help_btn").click(function(){
+            $(".help").show();
+            $(".chat").hide();
+        });
+
+    });
+
+        // responsive menu open and close
+
+        $(".res_menu_icon").click(function(){
+            console.log('hello');
+            $(".res_menu").toggle();
+
+            $(".res_menu ul li").click(function(){
+                $(this).find("ul").toggle();
+            });
+
+        });
+
+
+    
+
+  });
